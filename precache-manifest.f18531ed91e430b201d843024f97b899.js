065db58ed99dc35f3ffa7b9c2762d52356fbc9c0
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2021-02-01 05:22:44 -0500

    Updates

---
+++ precache-manifest.f18531ed91e430b201d843024f97b899.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c9669ae0dcd50f0f4b4",
+    "url": "/node-express-mongodb-reactjs-graphql/static/css/main.bab99e69.chunk.css"
+  },
+  {
+    "revision": "2c9669ae0dcd50f0f4b4",
+    "url": "/node-express-mongodb-reactjs-graphql/static/js/main.36b0352f.chunk.js"
+  },
+  {
+    "revision": "7f6caa1aea240ecbf299",
+    "url": "/node-express-mongodb-reactjs-graphql/static/js/runtime~main.dfad1e0b.js"
+  },
+  {
+    "revision": "d34be3a0308a19f62b35",
+    "url": "/node-express-mongodb-reactjs-graphql/static/css/2.5d8a6eb4.chunk.css"
+  },
+  {
+    "revision": "d34be3a0308a19f62b35",
+    "url": "/node-express-mongodb-reactjs-graphql/static/js/2.3b408aff.chunk.js"
+  },
+  {
+    "revision": "f5bc7caf0caa28923884d129cc398b7e",
+    "url": "/node-express-mongodb-reactjs-graphql/index.html"
+  }
+];
